fb5a6b14c8dadd426835f7d218f03fcef7a7f2c3
odofin Gbadamosi <odofing@gmail.com>
2020-11-29 16:32:25 +0100

    Modified

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,35 +1,52 @@
-import React from 'react'
-import { NetlifyForm, Honeypot } from 'react-netlify-forms'
+import React from 'react';
 
-const ContactForm = () => (
 
-  <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
-    {({ handleChange, success, error }) => (
-      <>
-        <Honeypot />
-        {success && <p>Thanks for contacting us!</p>}
-        {error && (
-          <p>Sorry, we could not reach our servers. Please try again later.</p>
-        )}
-        <div>
-          <label htmlFor='name'>Name:</label>
-          <input type='text' name='name' id='name' onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor='message'>Message:</label>
-          <textarea
-            type='text'
-            name='message'
-            id='message'
-            rows='4'
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type='submit'>Submit</button>
-        </div>
-      </>
-    )}
-  </NetlifyForm>
-)
-export default ContactForm;
+function Contact() {
+    return (
+    <section className="mt-5 bg-green" >
+    <div className="container" >
+      <div className="row " >
+        <form action="/contact" method="post" name="contact" className="col-lg-12">
+        <input type="hidden" name="contact" value="contact" />
+          <div className="card p-4  bg-light">
+          <h3 className="card-title text-center text-warning">Get In Touch</h3>
+            <div className="card-body">
+             
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="form-group">
+                 <input type="text" className="form-control" id="Name" placeholder="Name" required/>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <input type="email"className="form-control" name="e-mail" id="email" placeholder="Email" required/>
+                  </div>
+                </div>
+                </div> 
+              </div>
+              
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <textarea className="form-control" id="instructions" placeholder="Message" required></textarea>
+                  </div>
+                </div>
+               
+              {/* <div className="col-lg-12" id="field">
+                  <div data-netlify-recaptcha="true" className="form-group">
+                  </div>
+                </div> */}
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <button type="submit" className="btn btn-warning btn-block text-dark">SUBMIT </button>
+                  </div>
+                </div>
+            </div>
+        </form>
+      </div>
+      </div>
+  </section>
+    )
+}
+
+export default Contact;
